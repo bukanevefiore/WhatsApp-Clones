97b38daaa692f7e279992411829c39e9bccde6ad
Melis Nur KILIÇ <fiorebucaneve22@gmail.com>
2021-04-08 01:08:00 +0300

    firebaseconn

--- whatsapp/lib/screens/chats_page.dart
+++ whatsapp/lib/screens/chats_page.dart
@@ -1,8 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatsPage extends StatelessWidget {
+  const ChatsPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Firebase.initializeApp();  // gerekli
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('whats').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if(snapshot.hasError){
+          return Text('Error: ${snapshot.error}');
+        }
+        if(snapshot.connectionState != ConnectionState.waiting){
+          //return Center(child: CircularProgressIndicator(backgroundColor: Colors.tealAccent));
+          return Text('Loading...');
+        }
+        return ListView(
+          children: snapshot.data.docs
+              .map((doc) => ListTile(
+                    title: Text(doc['name']),
+                    subtitle: Text(doc['message']),
+                   ))
+              .toList(),
+        );
+      },
+    );
   }
 }
